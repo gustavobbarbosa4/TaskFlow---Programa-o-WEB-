69626231bd153f1aa136e7af2d3a113b524d8436
gustavobbarbosa4 <gustavobbarbosa4@gmail.com>
2026-06-17 19:54:50 -0300

    Add Middleware nas rotas de autenticação

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,14 +4,14 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
-router.get('/login', authController.showLogin);
+router.get('/login', authMiddleware,authController.showLogin);
 
-router.post('/login', authController.login);
+router.post('/login', authMiddleware,authController.login);
 
-router.get('/register', authController.showRegister);
+router.get('/register', authMiddleware,authController.showRegister);
 
-router.post('/register', authController.register);
+router.post('/register', authMiddleware,authController.register);
 
-router.get('/logout', authController.logout);
+router.get('/logout', authMiddleware,authController.logout);
 
 module.exports = router;
