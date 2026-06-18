9a98c514375260e357bd130d162bce108f0afc78
Gustavo Barbosa <gustavobbarbosa4@gmail.com>
2026-06-18 19:54:17 -0300

    rota tasks protegidas novamente

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,10 +1,10 @@
 function authMiddleware(req, res, next) {
+
     if (!req.session.user) {
         return res.redirect('/auth/login');
     }
 
     res.locals.user = req.session.user;
-
     next();
 }
 
